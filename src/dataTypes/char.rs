ca027fa0dc3de65b55ba075431b669518bd7ac48
devfabien <ish001fabi@gmail.com>
2024-10-23 11:40:44 +0200

    feat: add the char datatype

---
+++ src/dataTypes/char.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+    println!("heart eyed cat: {}", heart_eyed_cat.len_utf8());
+}
