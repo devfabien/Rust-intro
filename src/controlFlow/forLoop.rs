4e21887d24b2d1701c35971c07ef95c0ee2de004
devfabien <ish001fabi@gmail.com>
2024-10-29 08:44:25 +0200

    feat: create the for loop implementation

---
+++ src/controlFlow/forLoop.rs
@@ -0,0 +1,22 @@
+// while loop vs for loop
+
+//while loop
+fn main() {
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+}
+
+//for loop
+fn main() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+}
