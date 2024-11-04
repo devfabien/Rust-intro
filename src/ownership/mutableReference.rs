8b3168e39ff0e20ed25d6a50279e9a65c4f5a9ce
devfabien <ish001fabi@gmail.com>
2024-11-04 18:07:49 +0200

    feat: restrictions with mutable references

--- src/ownership/mutableReference.rs
+++ src/ownership/mutableReference.rs
@@ -10,3 +10,17 @@ fn main() {
 fn change(some_string: &mut String) {
     some_string.push_str(", world");
 }
+
+// The &mut s syntax lets us create a mutable reference to s
+// which allows us to modify s
+// But mutable references have one big restriction: you can only have one mutable reference to a particular piece of data in a particular scope
+
+// This code will not compile because it has two mutable references to s in the same scope:
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    println!("{}, {}", r1, r2);
+}
