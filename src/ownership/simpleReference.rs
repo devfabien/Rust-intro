155e58a234aba8b95114fa42092ad6243760fa85
devfabien <ish001fabi@gmail.com>
2024-11-04 17:46:41 +0200

    feat: add anexample that shows what happens when you try to modify a referred value

--- src/ownership/simpleReference.rs
+++ src/ownership/simpleReference.rs
@@ -4,8 +4,17 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("The length of '{s1}' is {len}.");
+
+    // change(&s1);: this will give an error
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+// when you try to modify the value of a reference, you'll get a compile-time error
+// because references are immutable by default
+
+fn change(some_string: &String) {
+    some_string.push_str(", world");
 }
