e664da044a378b50ceb551950090e7597dd8d2be
devfabien <ish001fabi@gmail.com>
2024-11-19 10:02:15 +0200

    feat: add a new method to calculate the area of a rectangle using tuples

--- src/structs/rectangle.rs
+++ src/structs/rectangle.rs
@@ -12,3 +12,17 @@ fn _main() {
 fn _area(width: u32, height: u32) -> u32 {
     width * height
 }
+
+// Second method: using tuples.
+fn _main() {
+    let rect1 = (30, 50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        _area(rect1)
+    );
+}
+
+fn _area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
