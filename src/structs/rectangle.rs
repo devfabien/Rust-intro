54e2dca1ffc269140d97d2d0dfe3df06c9c54bb3
devfabien <ish001fabi@gmail.com>
2024-11-19 10:03:37 +0200

    feat: add a new method to calculate the area of a rectangle using structs

--- src/structs/rectangle.rs
+++ src/structs/rectangle.rs
@@ -26,3 +26,25 @@ fn _main() {
 fn _area(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
 }
+
+// Third method: using structs.
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
