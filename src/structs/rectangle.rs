77572157ddbc902ebad9df8294f30a8534db636f
devfabien <ish001fabi@gmail.com>
2024-11-19 10:04:47 +0200

    refactor: refactoring the codes

--- src/structs/rectangle.rs
+++ src/structs/rectangle.rs
@@ -1,31 +1,31 @@
 // First method: using variables.
-fn _main() {
-    let width1 = 30;
-    let height1 = 50;
+// fn main() {
+//     let width1 = 30;
+//     let height1 = 50;
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        _area(width1, height1)
-    );
-}
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(width1, height1)
+//     );
+// }
 
-fn _area(width: u32, height: u32) -> u32 {
-    width * height
-}
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
 
 // Second method: using tuples.
-fn _main() {
-    let rect1 = (30, 50);
+// fn main() {
+//     let rect1 = (30, 50);
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        _area(rect1)
-    );
-}
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(rect1)
+//     );
+// }
 
-fn _area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
-}
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
 
 // Third method: using structs.
 struct Rectangle {
